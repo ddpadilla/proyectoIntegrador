0363ce530388217ba3de678f06f0698259d645a4
Dennis <ddpadilla89@gmail.com>
2023-07-28 12:57:46 -0600

    Añadiendo datos del jugardo al main

---
+++ main.py
@@ -0,0 +1,7 @@
+""" Crear el archivo main del proyecto
+    Pedir el nombre del jugador por teclado
+    Imprimir un mensaje de bienvenida con el nombre."""
+
+print("Digite el nombre del jugador: ")
+nombre = input()
+print(f"Hola {nombre}, bienvenido!!")
