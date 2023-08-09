d23f0414f4689ce00cf6b440b8d3d647212bc8dd
Dennis <ddpadilla89@gmail.com>
2023-08-09 14:50:26 -0600

    Añadiendo bucle para digitar caracteres

--- main.py
+++ main.py
@@ -2,6 +2,18 @@
     Pedir el nombre del jugador por teclado
     Imprimir un mensaje de bienvenida con el nombre."""
 
+import readchar
+
 print("Digite el nombre del jugador: ")
 nombre = input()
 print(f"Hola {nombre}, bienvenido!!")
+print("**Presione una tecla para continuar y")
+print("presione la tecla UP para finalizar**")
+
+
+while True:
+    caracter = readchar.readkey()   # la variable caracter almacena la tecla presionada.
+    print("Tecla presionada: ", caracter) 
+    if readchar.readkey() == "\x1b[A":
+        print("Programa finalizado con la tecla UP")
+        break
